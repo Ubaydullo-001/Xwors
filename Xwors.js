6a2c0075110a23ce31968d550736bb6804a2bc36
Ubaydullo <209229073+Ubaydullo-001@users.noreply.github.com>
2025-12-20 15:47:07 +0500

    Update Xwors.js

--- Xwors.js
+++ Xwors.js
@@ -1,23 +1,37 @@
 const TelegramBot = require("node-telegram-bot-api");
 const { createClient } = require("@supabase/supabase-js");
-
-// Telegram bot (FAQAT 1 MARTA)
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-
-// Supabase
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
-
+  process.env.SUPABASE_KEY);
 bot.onText(/\/start/, async (msg) => {
   await supabase.from("users").insert({
     telegram_id: msg.from.id,
-    username: msg.from.username || null
+    username: msg.from.username || null });
+  bot.sendMessage(msg.chat.id, "Bot ishlayapti ✅");});
+const TelegramBot = require("node-telegram-bot-api");
+const token = process.env.BOT_TOKEN; // Railway / Render env dan oladi
+const bot = new TelegramBot(token, { polling: true });
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  if (!text || text.startsWith("/")) return;
+  const link = `@Olimov`;
+  bot.sendMessage(chatId, `🔍 "${text}" uchun havola:`, {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "👉 Ochish",
+            url: link
+          }
+        ]
+      ]
+    }
   });
-
-  bot.sendMessage(msg.chat.id, "Bot ishlayapti ✅");
 });
+console.log("🤖 Bot ishga tushdi");
+
 // bot.on("message", async (msg) => {
 //   const chatId = msg.chat.id.toString();
 
